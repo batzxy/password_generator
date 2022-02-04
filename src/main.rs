e47a4078c643c87235dee9b8dc1803dad8a5c1c3
root <root@debian-BULLSEYE-live-builder-AMD64>
2022-02-04 10:20:11 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+use std::env;
+
+fn main () {
+
+    let charset = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    
+    let args: Vec<String> = env::args().collect();
+
+    let password_length: i32 = args[1].parse().unwrap();
+    
+    let mut counter: i32 = 1;
+    
+    while counter <= password_length {
+        print!("{}", charset[rand::thread_rng().gen_range(0, charset.len())]);
+        counter += 1;
+    }
+    println!("");
+}
+
